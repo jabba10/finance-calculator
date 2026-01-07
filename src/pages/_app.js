c74083886f0ceb493f57b0429c599353de704b88
jabba10 <isatakamara1001@gmail.com>
2026-01-07 13:29:49 +0200

    a

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,19 @@
-// src/pages/_app.js (Recommended)
+// src/pages/_app.js
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import './globals.css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const canonicalUrl = `https://www.financecalculatorfree.com${router.asPath}`;
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
       <Navbar />
       <main className="app-wrapper">
         <Component {...pageProps} />
