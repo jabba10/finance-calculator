4c3a1c673349d7e56289ce6e5171d4eea2d86bd3
jabba10 <sahrjabba100@gmail.com>
2026-01-09 22:02:04 +0200

    d

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 // src/pages/_app.js
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import './globals.css';
@@ -8,12 +10,57 @@ import './globals.css';
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const canonicalUrl = `https://www.financecalculatorfree.com${router.asPath}`;
+  
+  const GA_MEASUREMENT_ID = 'G-2E7Q5ZXC2D';
+
+  // Track page views
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+        anonymize_ip: true, // GDPR compliance
+      });
+    };
+    
+    // Track initial load
+    handleRouteChange(router.asPath);
+    
+    // Track route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events, router.asPath]);
 
   return (
     <>
       <Head>
-        <link rel="canonical" href={canonicalUrl} />
+        <link rel="canonical" href={canonicalUrl} key="canonical" />
       </Head>
+      
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+              anonymize_ip: true
+            });
+          `,
+        }}
+      />
+      
       <Navbar />
       <main className="app-wrapper">
         <Component {...pageProps} />
